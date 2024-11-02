e8e3547177940a352620093633684c90470d23e5
RahmaFahrurrozi <zfahrur150105@gmail.com>
2024-11-02 18:45:17 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'ca022591a1bac120a20ac65a1405236d',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
